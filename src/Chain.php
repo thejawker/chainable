f20889b22dc4389ffc3f5f32b8b2b587b8753d9a
Bram Veerman <veerman@stormdigital.nl>
2017-09-27 14:58:05 +0200

    Code clean up

--- src/Chain.php
+++ src/Chain.php
@@ -7,6 +7,9 @@ class Chain
     private $instance = null;
     private $escape = false;
 
+    /**
+     * Initializes the Chain.
+     */
     public function __construct()
     {
         $pars = func_get_args();
@@ -26,23 +29,49 @@ class Chain
         return $this->escape ? $result : $this;
     }
 
+    /**
+     * Gets the actual original instance back.
+     * Especially nice if you want to check
+     * for class_basename() or something
+     * in that direction or something
+     *
+     * @return mixed|null
+     */
     public function instance()
     {
         return $this->instance;
     }
 
+    /**
+     * Turns the chainable off until turned on
+     * again by using the unescape method.
+     *
+     * @return $this
+     */
     public function escape()
     {
         $this->escape = true;
         return $this;
     }
 
+    /**
+     * Turns the chainable on until turned off
+     * again by using the escape method.
+     *
+     * @return $this
+     */
     public function unescape()
     {
         $this->escape = false;
         return $this;
     }
 
+    /**
+     * Gets the property magically.
+     *
+     * @param $name
+     * @return mixed
+     */
     public function __get($name)
     {
         return $this->instance->$name;
